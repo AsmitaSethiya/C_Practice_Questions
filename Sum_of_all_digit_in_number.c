8afffebb38f547a223f1e1c1bd8c648a70137606
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-06-24 10:40:09 +0530

    Create Sum_of_all_digit_in_number.c

---
+++ Sum_of_all_digit_in_number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+    int n, r, sum = 0;
+    
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    
+    while(n != 0)
+    {
+        r = n % 10;
+        sum = sum + r;
+        n = n / 10;
+    }
+    
+    printf("The sum of all digit is : %d", sum);
+}
