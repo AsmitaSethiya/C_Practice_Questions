6dcf577603dc43b3fdbdde42fdb641aa21d42ffa
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-07-01 13:07:36 +0530

    Update Areas_Of_Triangle__Square_Circle_Rectangle_Parallelogram.c

--- Areas_Of_Triangle__Square_Circle_Rectangle_Parallelogram.c
+++ Areas_Of_Triangle__Square_Circle_Rectangle_Parallelogram.c
@@ -3,7 +3,7 @@
 void main()
 {
     int choice;
-    float base, height, area, width, length, radius, side;
+    float base, height, area, width, radius, side;
     printf("Enter\n1 for Triangle\n2 for Rectangle\n3 for Circle\n4 for Square\n5 for Parallelogram\n");
     scanf("%d", &choice);
     
@@ -18,8 +18,8 @@ void main()
             printf("Area of Triangle is: %f", area);
             break;
         case 2:
-            printf("Enter width and length of Rectangle: ");
-            scanf("%f %f", &width, &length);
+            printf("Enter width and height of Rectangle: ");
+            scanf("%f %f", &width, &height);
             
             area = (float)width * length;
             printf("Area of Rectangle is: %f", area);
