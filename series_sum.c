a203b5bf959b58c1deeb76f3ff48f1bf327cd322
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-07-03 10:10:18 +0530

    Create series_sum.c

---
+++ series_sum.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void main()
+{
+    int i,n;
+    float sum = 0;
+    
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+    
+    for(i = 1; i <= n; i++)
+    {
+        sum += (float)1/i;
+    }
+    
+    printf("The value of\n");
+    
+    for(i = 1; i <= n; i++)
+    {
+        if(i < n)
+        {
+            printf("1/%d+ ", i);
+        }
+        else
+        {
+            printf("1/%d = ", i);
+        }
+    }
+    
+    printf("%f\n",sum);
+}
