4655991b6986914cd24c6ea7dbe6b1cf4a7667b8
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-07-07 10:27:42 +0530

    Create count_vowel_in_sentence.c

---
+++ count_vowel_in_sentence.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+//#include<String.h>
+
+void main()
+{
+    char s[100];
+    
+    int count = 0;
+    
+    printf("Enter  a sentence : ");
+    gets(s);
+    
+    for(int i = 0; i < strlen(s); i++)
+    {
+        if(s[i] =='a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] =='A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+        {
+            count++;
+        }
+    }
+    
+    if(count == 0)
+    {
+        printf("There are no vowel\n");
+    }
+    else
+    {
+    printf("In the given sentence there are %d vowels", count);
+
+    }    
+}
