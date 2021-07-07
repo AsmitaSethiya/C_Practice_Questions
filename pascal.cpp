6ab97e33a756faf2626ee4d5a0606293c7963b3e
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-07-07 11:42:44 +0530

    Create pascal.cpp

---
+++ pascal.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+{
+    int factorial =  1;
+
+    for(int k = 1; k <= n; k++)
+    {
+        factorial = factorial * k;
+    }
+
+    return factorial;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j <= i; j++)
+        {
+            cout<<fact(i) / (fact(j) * fact(i - j))<<" ";
+        }
+
+        cout<<endl;
+
+    }
+}
