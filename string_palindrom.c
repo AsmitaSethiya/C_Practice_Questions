af34e233d53d8ed17a60eec23a299343c461d680
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-07-08 10:51:56 +0530

    Create string_palindrom.c

---
+++ string_palindrom.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+    int i , j = 0;
+    
+    char str1[100], str2[100] = {0};
+    
+    printf("Enter a string to know whether it is palindrom or not: ");
+    gets(str1);
+    
+    for(int i = strlen(str1) - 1; i >= 0; i--)
+    {
+        str2[j] = str1[i];
+        j++;
+    }
+    
+    str2[j] = '\0';
+    
+    printf("The reverse of the given string is: \n");
+    puts(str2);
+    
+    if(strcmp(str1, str2) == 0)
+    {
+        printf("The given string is palindrom\n");
+    }
+    else
+    {
+        printf("The given string is not a palindrom\n");
+    }
+}
